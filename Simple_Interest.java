6dd6de679e2a313ee3c8e79d6b3747adf6668115
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2021-08-23 23:11:53 +0530

    Add files via upload

---
+++ Simple_Interest.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Simple_Interest {
+    public static void main(String[] args) {
+        double result,interest;
+        int principal,time;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Principal,interest,time");
+        principal = input.nextInt();
+        interest = input.nextInt();
+        time = input.nextInt();
+        result = (principal*interest*time)/100;
+        System.out.println("Simple Interest is " + result);
+    }
+}
