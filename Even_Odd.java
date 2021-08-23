87d70df07786a6115981504032ab91b7523bb01f
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2021-08-23 23:00:30 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int number;
+            number = input.nextInt();
+            if(number%2 == 0){
+                System.out.println("Number is Even");
+            }
+            else{
+                System.out.println("Number is Odd");
+            }
+        }
+    }
