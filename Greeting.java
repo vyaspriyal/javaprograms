315b74b01a50f64a3dc665b5f62768a6c95ceb15
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2021-08-23 23:04:38 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = "";
+        System.out.println("Enter your name ");
+        name = input.next();
+        System.out.println("Welcome " + name);
+    }
+}
