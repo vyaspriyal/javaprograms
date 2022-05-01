8bd2e38d82e8c20fd4bdf97521560b1ecf000557
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2022-05-01 13:49:07 +0530

    Create  powerLinear.java

---
+++ recursion/powerLinear.java
@@ -0,0 +1,15 @@
+public class powerLinear {
+    public static void main(String[] args) {
+        int ans = powerLinear(2,6);
+        System.out.println(ans);
+    }
+    public static int powerLinear(int x , int n){
+        int ans;
+        if(n == 0){
+            return  1;
+        }
+        ans = x * powerLinear(x,n-1);
+        return ans;
+
+    }
+}
