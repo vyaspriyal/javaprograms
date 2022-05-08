1f8833da735cbd053ba1bbce6eaa853527e023b0
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2022-05-08 22:02:47 +0530

    Update allIndicesArray.java

--- recursion/Basics/allIndicesArray.java
+++ recursion/Basics/allIndicesArray.java
@@ -8,29 +8,31 @@ public class allIndicesArray{
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        int [] originalArray  = {2,3,6,9,8,6,2,3,6,4};
+        int [] originalArray  = {2,3,6,9,8,6,2,3,6,6};
         int x = 6;
         int [] arr = find(originalArray,x,0,0);
         System.out.println(Arrays.toString(arr));
     }
 
     public static int[] find(int [] originalArray,int x,int inx,int fsf){
-
+        if(inx == originalArray.length){
+            return new int[fsf];
+        }
         if(originalArray[inx] == x){
 
             fsf = fsf+ 1;
         }
 
-        if(inx == originalArray.length-1){
-            return new int[fsf];
-        }
+
 
 
 
        int [] ans = find(originalArray, x, inx + 1, fsf);
+
        if(x == originalArray[inx]){
+
            ans[fsf-1] = inx;
-           fsf = fsf -1;
+           fsf = fsf - 1;
        }
 
 
